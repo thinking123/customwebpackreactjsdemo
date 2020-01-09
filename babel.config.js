991414893ed50310cc873475a42f337c96f815ba
liangqing <liangq@weipaitang.com>
2020-01-09 17:01:23 +0800

    config

---
+++ babel.config.js
@@ -0,0 +1,12 @@
+const presets = [
+    [
+        "@babel/preset-react",
+        {
+          "pragma": "dom", // default pragma is React.createElement
+          "pragmaFrag": "DomFrag", // default is React.Fragment
+          "throwIfNamespace": false // defaults to true
+        }
+      ]
+  ];
+  
+  module.exports = { presets };
